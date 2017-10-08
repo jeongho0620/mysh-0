0934fbc3b400240f3c8ea6983f1e5421382e17c8
jeongho <nature6807@naver.com>
2017-10-08 23:49:54 +0900

    complete utils.c code

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,30 @@
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
 #include "utils.h"
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
+	char *new=(char*)malloc(sizeof(char));
+	strcpy(new,command);
+	char * a=strtok(new," \t\n");
+	int count=0;
+	int i=0;
+
+	*argv=(char**)malloc(sizeof(char));
+	**argv=(char*)malloc(sizeof(char));
+
+	while(a!=NULL)
+	{
+		count++;
+		*((*argv)+i)=(char*)malloc(sizeof(char));
+		strcpy(*((*argv)+i),a);
+		i++;
+		a=strtok(NULL," ");
+	}
+	
+	*argc=count;
+	free(a);
   // TODO: Fill this!
 }
